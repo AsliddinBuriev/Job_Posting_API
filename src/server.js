9606137715f4d2d757ddc9a1e4192fd743a68397
abdulvokhid-ochilov <asliddin00buriev@gmail.com>
2021-12-03 02:38:43 +0900

    restart server with pm2

--- src/server.js
+++ src/server.js
@@ -1,14 +1,2 @@
-const express = require('express')
-const app = express()
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'hello world!'
-  })
-})
-
-app.listen(3000, () => {
-  console.log('The application is running on port 3000!');
-})
 
