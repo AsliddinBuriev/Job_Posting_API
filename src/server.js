0d97f99819ce232e958c1afe9e081d3fb424cec9
abdulvokhid-ochilov <asliddin00buriev@gmail.com>
2021-12-03 01:54:02 +0900

    test pipeline

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
-
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    status: success,
+    data: 'test pipeline!'
+  })
+})
 
 app.listen(3000, () => {
   console.log('The application is running on port 3000!');
