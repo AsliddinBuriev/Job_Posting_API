013892a82e9b95fe96059110691fe8cf950287b2
Asliddin Buriev <asliddin00buriev@gmail.com>
2022-01-05 23:42:26 +0900

    add reset && update user data and password

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -2,9 +2,12 @@ import catchAsyncErr from '../utils/catchAsyncErr.js';
 import User from './../Models/userSchema.js';
 import { sign, verify } from '../utils/jwt.js';
 import MakeError from './../utils/MakeError.js';
+import sendMail from './../utils/mail.js';
+import crypto from 'crypto';
 
 /******** SIGN UP *******/
 export const signup = catchAsyncErr(async (req, res, next) => {
+  //1.create user
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -12,9 +15,9 @@ export const signup = catchAsyncErr(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     lastPwChanged: req.body.lastPwChanged,
   });
-  //create token
+  //2.create token
   const token = await sign({ id: newUser._id });
-  //send response
+  //3.send response
   res.status(201).json({
     status: 'success',
     token,
@@ -45,6 +48,25 @@ export const login = catchAsyncErr(async (req, res, next) => {
   });
 });
 
+/********  UPDATE PASSWORD *******/
+export const updatePassword = catchAsyncErr(async (req, res, next) => {
+  //get the current user
+  const user = await User.findById(req.user._id).select('+password');
+  //2. check user password
+  if (!(await user.isPasswordCorrect(req.body.oldPassword, user.password)))
+    return next(new MakeError('Your password is not correct', 401));
+  //3. update password
+  user.password = req.body.newPassword;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  //4. login again
+  const token = await sign({ id: user._id });
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
+
 /******** PROTECT *******/
 export const protect = catchAsyncErr(async (req, res, next) => {
   //1.check if token exists in req.headers
@@ -69,4 +91,73 @@ export const protect = catchAsyncErr(async (req, res, next) => {
     );
   req.user = loggedUser;
   next();
+});
+
+/******** FORGOT PASSWORD  *******/
+export const forgotPassword = catchAsyncErr(async (req, res, next) => {
+  //1. Get user based on email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    return next(new MakeError(`There is no account with this email!`, 400));
+
+  //2. Generate the random reset token with crypto module, encrypt and save token into db
+  const token = await user.generateResetPwToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. Send it to user's email along with a url
+  try {
+    await sendMail({
+      email: user.email,
+      subject: 'Reset your password!',
+      text: `Please go the url to reset your password: ${req.protocol}://${req.hostname}/api/v1/user/reset-password/${token}\nThe url is valid for 10 minutes!`,
+    });
+  } catch (err) {
+    user.pwResetToken = undefined;
+    user.pwResetTokenExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new MakeError(
+        'There was an error sending the email. Please try again!',
+        500
+      )
+    );
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: `Reset password url is sent to your email!`,
+  });
+});
+
+/******** PASSWORD RESET *******/
+export const resetPassword = catchAsyncErr(async (req, res, next) => {
+  //1. Get user based on token
+  const pwResetToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    pwResetToken: pwResetToken,
+    pwResetTokenExpire: { $gt: Date.now() },
+  });
+  //2. If token is not expired and user is exist, update the user password
+  if (!user)
+    return next(
+      new MakeError('Your token is invalid or expired. Please try again!', 400)
+    );
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.pwResetToken = undefined;
+  user.pwResetTokenExpire = undefined;
+  await user.save();
+
+  //4. create jwt token and log the user in
+  const token = await sign({ id: user._id });
+  res.status(200).json({
+    status: 'success',
+    message: 'You are logged in with your new password!',
+    token,
+  });
 });
